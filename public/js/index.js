806b15a560ec29a8b34448bee47701c5694a7be9
David Gaynor <david.gaynor@students.olin.edu>
2015-10-05 17:37:46 -0700

    added all three sections, still need to add bios, and figure out colors and logo

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,13 @@ function setContentHeight() {
 
 	document.getElementById("content").style.height = viewportHeight * numberOfSections;
 	document.getElementById("section-1").style.height = viewportHeight;
-	document.getElementById("section-2").style.height = viewportHeight;
+	document.getElementById("section-2").style["min-height"] = viewportHeight;
+	document.getElementById("section-3").style["min-height"] = viewportHeight;
+
+	var sections = document.getElementsByClassName("section");
+	for (i = 0; i < sections.length; i ++)  {		
+		sections[i].style.visibility = "visible";
+	}
 }
 
 window.onresize = setContentHeight;
